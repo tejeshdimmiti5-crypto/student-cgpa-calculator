f1c0b1002ef03162d63a20452311e5252faa3349
tejeshdimmiti5-crypto <tejeshdimmiti5@gmail.com>
2026-09-18 20:37:30 +0530

    Remove superseded JARVIS scaffold files

--- jarvis/python-core/app/core/planner.py
+++
@@ -1,8 +0,0 @@
-class Planner:
-    """Initial deterministic planner; LLM planning will be plugged in here."""
-
-    def create_plan(self, command: str) -> list[str]:
-        command = command.strip()
-        if not command:
-            return []
-        return [command]
